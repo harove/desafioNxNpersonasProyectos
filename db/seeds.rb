0695139e6692896d30d1b11692c85cebc2696fec
edgar <harove@gmail.com>
2019-07-22 22:25:42 -0400

    proyectos y personas pertenecientes a proyectos

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,26 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Employee.destroy_all
-Project.destroy_all
+User.destroy_all
+Todo.destroy_all
 
 6.times do |i|
-         Employee.create({
+         User.create({
          name:"name #{i+1}" ,
+         photo:"https://picsum.photos/id/#{i+1}/200/200",
+         email:"email#{i+1}@desafio.cl" 
+         password: "123456"
+         password_confirmation: "123456"
          })
 end      #6 Employees
 
 
 
 10.times do |i|
-   p=Project.create({
-   name:"title #{i+1}",
+   t=Todo.create({
+   title:"title #{i+1}",
    }) #1Project
    4.times do |i|
-      p.employees << Employee.order('RANDOM()').first
+      t.users << Todo.order('RANDOM()').first
    end # 4 Proyectos
 end   
